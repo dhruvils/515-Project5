248a9631fd8de93ffebd69465b8b9644db48ce54
Justin Chiu <justc@seas.upenn.edu>
2014-12-18 04:13:12 -0500

    lol

--- def_interp.m
+++ def_interp.m
@@ -1,4 +1,4 @@
-function [trans] = simpl_def_interp(A, B)
+function [trans] = def_interp(A, B)
 % need to take out V first?
 lA = logm(A)
 lB = logm(B)
@@ -8,6 +8,8 @@ simB = to_sim(lB)
 
 T = 10;
 trans = cell(T,1);
+% use decasteljau subdivision then convert
+decas = 0;
 for t = 1:T
-    trans{t} = expm(to_big_sim(t/T * simB + (1-t/T) * simA));
+    trans{t} = expm(to_big_sim(decas{t}));
 end
